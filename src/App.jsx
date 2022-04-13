f24812ae150a704b387ff8c9227b169616bd251b
Sunny9899 <suryanshpajiala@gmail.com>
2022-04-13 14:48:55 -0700

    13Apr

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import './App.css';
 
 import {Filter} from "./components/Filters/Filters";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 
 
 function App() {
-  const [filter,setFilter]=useState("");
+ // const [filter,setFilter]=useState("");
   const [prod,setProd]=useState([]);
 //useEffect(() =>{filterData()},[])
-
+/*
   var filterData=(exp)=>{
     setFilter(exp);
     //return filter;
     //console.log(filter);
   }
-
+*/
   return (
     <div className="App">
       <b style={{"fontSize":"40px"}}>Filter: </b>
-      <Filter filterData={filterData} filter={filter} prod={prod} setProd={setProd}/>      
+      <Filter /*filterData={filterData} filter={filter} */prod={prod} setProd={setProd}/>      
 
 
     </div>
