60374b6cbd2ab052bc7a1824c2a088853d0482dc
Sunny9899 <suryanshpajiala@gmail.com>
2022-05-02 14:46:34 +0530

    old files removed

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-export const Button = ({Clicked,txt,param1,param2}) => {
-    return (
-      <button
-        onClick={()=>{Clicked(param1,param2)}}
-      >
-        {txt}
-      </button>
-    );
-  };
